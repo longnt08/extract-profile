c0d69b70db62f0dd261462b4afe8b35e22ba9131
longnt08 <thanhlong0j@gmail.com>
2025-01-10 00:02:44 +0700

    replace python 3.13 by python 3.12

--- src/processor/extract.py
+++ src/processor/extract.py
@@ -1,10 +1,10 @@
-from http.client import responses
-
-import requests
-import openai
 import os
+import requests
+from transformers import pipeline
+from http.client import responses
 from dotenv import load_dotenv
 from pyexpat.errors import messages
+
 
 load_dotenv()
 
@@ -20,25 +20,8 @@ def fetch_html(url):
         print(f"Error fetching URL: {e}")
         return None
 
-def send_to_chatgpt(html_content, prompt):
-    openai.api_key = OPEN_API_KEY
 
-    try:
-        response = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are an assistant skilled at processing HTML data."},
-                {"role": "user", "content": f"HTML content: \n{html_content}"},
-                {"role": "user", "content": prompt}
-            ]
-        )
-        return response.choices[0].message.content
-    except Exception as e:
-        print(f"Error communicating with chatgpt: {e}")
-        return None
 
 if __name__ == '__main__':
     html_content = fetch_html("https://math.uchicago.edu/~ngo/")
-    prompt = "Get name, title from this html data and return json format of result"
-
-    result = send_to_chatgpt(html_content, prompt)
+    prompt = '''Get name, title from this html data and return json format of result: {html_content}'''
