39cd65c6cb793884333e39ab6099a6aa9a34693e
longnt08 <thanhlong0j@gmail.com>
2025-02-27 23:37:11 +0700

    thay uuid bang md5

--- src/API/crawler/crawler_repository.py
+++ src/API/crawler/crawler_repository.py
@@ -1,4 +1,5 @@
-import uuid
+import hashlib
+import datetime
 from API.common.ElasticClient import ElasticClient
 
 class CrawlerRepository:
@@ -6,10 +7,12 @@ class CrawlerRepository:
         self.elastic_client = ElasticClient()
 
     async def save_raw_html(self, url, content):
-        doc_id = str(uuid.uuid4())
-        await self.elastic_client.insert_data("raw_homepages", doc_id, {"url": url, "html": content})
+        doc_id = hashlib.md5(url.encode()).hexdigest()
+        time_stamp = datetime.datetime.now().isoformat(timespec='seconds').replace("T", " ")
+        await self.elastic_client.insert_data("raw_homepages", doc_id, {"url": url, "time_stamp": time_stamp, "html": content})
         
         return doc_id
     
     async def save_parsed_html(self, raw_id, url, body):
-        await self.elastic_client.insert_data("parsed_homepages", raw_id, {"url": url,  "body": body})
+        time_stamp = datetime.datetime.now().isoformat(timespec='seconds').replace("T", " ")
+        await self.elastic_client.insert_data("parsed_homepages", raw_id, {"url": url, "time_stamp": time_stamp, "body": body})
