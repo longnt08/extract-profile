93d134037d23f1b149f7184499b214f4e9142a46
longnt08 <thanhlong0j@gmail.com>
2025-02-28 08:41:11 +0700

    them get method

--- src/API/crawler/crawler_controller.py
+++ src/API/crawler/crawler_controller.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
 from API.crawler.crawler_service import CrawlerService
 from API.crawler.crawler_repository import CrawlerRepository
 
@@ -6,13 +7,30 @@ router = APIRouter()
 crawler_service = CrawlerService()
 crawler_repo = CrawlerRepository()
 
+# crawl raw & parsed HTML
 @router.post("/crawl")
 async def crawl(url: str):
-    content = await crawler_service.fetch_data(url)
-    parsed_html = await crawler_service.parse_html(content)
-    if content:
+    try:
+        content = await crawler_service.fetch_data(url)
+        parsed_html = await crawler_service.parse_html(content)
+        if not content:
+            raise HTTPException(status_code=404, detail="Can't get HTML data from this URL")
+
         doc_id = await crawler_repo.save_raw_html(url, content)
         await crawler_repo.save_parsed_html(doc_id, url, parsed_html)
 
-        return {"message": "Data saved successfully", "doc_id": doc_id}
-    return {"error": "Failed to fetch data"}
+        return JSONResponse(status_code=200, content={"_id": doc_id})
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
+
+# get parsed HTML    
+@router.get("/get_parsed_homepage/{id}")
+async def get_parsed_homepage(id: str):
+    try:
+        result = await crawler_repo.get_parsed_html(doc_id=id)
+        if not result:
+            raise HTTPException(status_code=404, detail="Content not found")
+
+        return JSONResponse(status_code=200, content={"body": result})
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
--- src/API/crawler/crawler_repository.py
+++ src/API/crawler/crawler_repository.py
@@ -6,7 +6,7 @@ class CrawlerRepository:
     def __init__(self):
         self.elastic_client = ElasticClient()
 
-    async def save_raw_html(self, url, content):
+    async def save_raw_html(self, url, content) -> str:
         doc_id = hashlib.md5(url.encode()).hexdigest()
         time_stamp = datetime.datetime.now().isoformat(timespec='seconds').replace("T", " ")
         await self.elastic_client.insert_data("raw_homepages", doc_id, {"url": url, "time_stamp": time_stamp, "html": content})
@@ -16,3 +16,15 @@ class CrawlerRepository:
     async def save_parsed_html(self, raw_id, url, body):
         time_stamp = datetime.datetime.now().isoformat(timespec='seconds').replace("T", " ")
         await self.elastic_client.insert_data("parsed_homepages", raw_id, {"url": url, "time_stamp": time_stamp, "body": body})
+
+    async def get_parsed_html(self, doc_id: str) -> str:
+       query = {
+          "query": {
+              "term": {
+                  "_id": doc_id
+              } 
+          } 
+       } 
+       parsed_html = await self.elastic_client.search_data(index="parsed_homepages", query=query)
+       result = parsed_html["hits"]["hits"][0]["_source"]["body"]
+       return result
